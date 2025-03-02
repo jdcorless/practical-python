f863cf4d89c0b8d2a62dba5bc8bb560f14f51a43
John D. Corless <jdcorless@gmail.com>
2025-03-02 08:38:45 -0600

    After completing section 1

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+num_payments = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.
+       # 12345678901234567890123456789012345678901234567890
+print("Payment Number Total Paid    Principal")
+while principal > 0:
+    principal = principal * (1 + rate/12) - payment
+    total_paid = total_paid + payment
+    if num_payments >= extra_payment_start_month and num_payments <= extra_payment_end_month:
+        total_paid = total_paid + extra_payment
+        principal = principal - extra_payment
+    if principal < 0:
+        total_paid = total_paid + principal
+        principal = 0
+    num_payments = num_payments + 1
+    print(f"{num_payments:<14} {round(total_paid, 2):<13} {round(principal, 2)}")
+if principal < 0:
+    total_paid = total_paid + principal
+principal = 0
+
+print(f'Total paid, {total_paid:.2f}')
+print(f'Number of Monthly Payments, { num_payments}')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(file):
+    'Read CSV file of stock, shares, price and compute total cost'
+    f = open(file, 'rt')
+    rows = csv.reader(f)
+    headers = next(rows)
+    total = 0
+    for row in rows:
+        try:
+            stock, shares, price = row
+            total = total + int(shares)*float(price)
+        except ValueError: # handle missing values
+            print("Couldn't parse", row)
+    f.close()
+    return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+total = portfolio_cost(filename)
+print(f"Total cost {total:.2f}")
